aace725fac1e21e95a37bc2d94281e54ac897873
Eduardo <duduprado294@gmail.com>
2023-02-08 16:31:59 -0300

    Faz o req 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -7,18 +8,21 @@ def fetch(url):
     try:
         response = requests.get(url, timeout=3)
         time.sleep(1)
-        if response.status_code != 200:
-            return None
-        else:
+        if response.status_code == 200:
             html_content = response.text
-        return html_content
+            return html_content
+        else:
+            return None
     except requests.Timeout:
         return None
 
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    updates = selector.css('h2.entry-title a ::attr(href)').getall()
+    print(updates)
+    return updates
 
 
 # Requisito 3
